3b6a07883003a0181214715bc5cdd60cc67ed674
Matthew Holloway <matthew.holloway@logicwerx.com>
2013-03-19 21:10:30 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MCJason::Application.config.secret_token = '9f166734898876aefda71ec7366f7f4357795d1f8d02ce4d9b60eb9d6905f26aabc6500e5d6c7a9f150b8992a0893c261ad378231c88f5858c3f1c3c3436250a'
